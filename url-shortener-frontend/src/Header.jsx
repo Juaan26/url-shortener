4a4656e5e3136abaae9a535da9c05a92e80f9dac
Juaan26 <Juaan26g@gmail.com>
2024-10-09 02:37:06 +0200

    creando diseño minimo del frontend

---
+++ url-shortener-frontend/src/Header.jsx
@@ -0,0 +1,11 @@
+import './Header.css'
+import { createRoot } from 'react-dom/client'
+
+
+createRoot(document.getElementById('root')).render(
+ <>
+ <nav className='header'> 
+    <p className='header-p'>WANDEV URL SHORTENER</p>
+ </nav>
+ </>
+)
