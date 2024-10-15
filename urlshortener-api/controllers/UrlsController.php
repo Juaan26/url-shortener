76dbb3287eb68e64d33be2ae1932afef0074e430
Juaan26 <Juaan26g@gmail.com>
2024-10-15 12:16:12 +0200

    cambio menor en codigo de url

--- urlshortener-api/controllers/UrlsController.php
+++ urlshortener-api/controllers/UrlsController.php
@@ -11,7 +11,7 @@ class UrlsController extends Controller
 
     public function shortenUrl($userId, $inputUrl)
     {
-        $shortenedUrl = "http://localhost:5173/ShortenedUrlPage/". substr(md5($inputUrl . time()), 0, 6); // Crear una URL acortada
+        $shortenedUrl = substr(md5($inputUrl . time()), 0, 8); // Crear una URL acortada
 
         if ($this->model->createUrl($userId, $inputUrl, $shortenedUrl)) {
             return $shortenedUrl; // Devuelve la URL acortada
