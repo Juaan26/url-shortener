b7de1fbda6d6356c3c2cc2f45392a2c480b0c052
Juaan26 <Juaan26g@gmail.com>
2025-07-08 20:39:19 +0200

    fix al index y comentado un console log de la página de redirección

--- frontend/src/RedirectToUrl.jsx
+++ frontend/src/RedirectToUrl.jsx
@@ -4,7 +4,7 @@ import axios from 'axios';
 
 export default function RedirectToUrl() {
     const { url } = useParams();
-    console.log(url);
+    // console.log(url);
 
     useEffect(() => {
         const redirect = async () => {
